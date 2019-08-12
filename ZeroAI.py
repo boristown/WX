b8e1f9f926ad94954da35914a1e9a7b7b659e16f
全王 <47687653+boristown@users.noreply.github.com>
2019-08-12 18:09:09 +0800

    迭代2：读取Prediction预测表

--- ZeroAI.py
+++ ZeroAI.py
@@ -5,6 +5,9 @@ import mysql.connector
 import mypsw
 
 def chat(input_text):
+  
+  output_text = ''
+  
   mydb = mysql.connector.connect(
     host=mypsw.wechatguest.host,
     user=mypsw.wechatguest.user,
@@ -14,9 +17,9 @@ def chat(input_text):
     )
 
   mycursor = mydb.cursor()
-  
+
   input_text = input_text.strip().upper()
-  
+
   select_alias_statment = "SELECT * FROM symbol_alias WHERE symbol = '" + input_text + "'"
 
   print(select_alias_statment)
@@ -24,15 +27,36 @@ def chat(input_text):
   mycursor.execute(select_alias_statment)
 
   alias_result = mycursor.fetchone()
-   
-  select_predictions_statment = "SELECT * FROM predictions WHERE symbol = '" + input_text + "'"
+  
+  if alias_result is None:
+    output_text = "市场'" + input_text + "'不存在！请尝试查询其它市场（如BTCUSD）！"
+    return output_text
+    
+  select_predictions_statment = "SELECT * FROM predictions WHERE symbol = '" + alias_result[1] + "'"
 
   print(select_predictions_statment)
 
   mycursor.execute(select_predictions_statment)
 
   predictions_result = mycursor.fetchone()
-  
-  print(predictions_result)
+  
+  if predictions_result is None:
+    output_text = "很抱歉，未找到市场'" + input_text + "'的预测信息！请尝试查询其它市场（如BTCUSD）！"
+    return output_text
+  
+  output_text = '市场编号:' + alias_result[1] + '\n'
+    + '预测日期：' + predictions_result[1] + '\n'
+    + '一天后：' + predictions_result[2] + '\n'
+    + '两天后：' + predictions_result[3] + '\n'
+    + '三天后：' + predictions_result[4] + '\n'
+    + '四天后：' + predictions_result[5] + '\n'
+    + '五天后：' + predictions_result[6] + '\n'
+    + '六天后：' + predictions_result[7] + '\n'
+    + '七天后：' + predictions_result[8] + '\n'
+    + '八天后：' + predictions_result[9] + '\n'
+    + '九天后：' + predictions_result[10] + '\n'
+    + '十天后：' + predictions_result[11] + '\n'.
     
-  return predictions_result
+  print(output_text)
+
+  return output_text
