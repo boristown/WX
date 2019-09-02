478a51497494b2bc19171781dc303d694d99cdf5
全王 <47687653+boristown@users.noreply.github.com>
2019-09-02 10:28:48 +0800

    迭代33：class Media

--- ZeroAI.py
+++ ZeroAI.py
@@ -114,3 +114,17 @@ def day_prediction_text(prediction_result):
 
 def score(prediction_result):
     return prediction_result * 2 - 1
+
+class Media(object):
+    def __init__(self):
+        register_openers()
+    #上传图片
+    def uplaod(self, accessToken, filePath, mediaType):
+        openFile = open(filePath, "rb")
+        param = {'media': openFile}
+        postData, postHeaders = poster.encode.multipart_encode(param)
+
+        postUrl = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=%s&type=%s" % (accessToken, mediaType)
+        request = urllib2.Request(postUrl, postData, postHeaders)
+        urlResp = urllib2.urlopen(request)
+        print urlResp.read()
