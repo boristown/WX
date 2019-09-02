9020a3fc80bcf2853568e61a9d06697ea53ce013
全王 <47687653+boristown@users.noreply.github.com>
2019-09-02 10:46:19 +0800

    迭代34：创建basic.py

---
+++ basic.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# filename: basic.py
+
+import urllib
+import time
+import json
+
+class Basic:
+  def __init__(self):
+    self.__accessToken = ''
+    self.__leftTime = 0
+    
+  def __real_get_access_token(self):        
+    appId = "wx5a0aed50fba86208"
+    appSecret = "022605a3b2658487462bbd432e185e36"
+    postUrl = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s" % (appId, appSecret))
+    urlResp = urllib.urlopen(postUrl)
+    urlResp = json.loads(urlResp.read())
+    self.__accessToken = urlResp['access_token']
+    self.__leftTime = urlResp['expires_in']
+    
+  def get_access_token(self):
+    if self.__leftTime < 10:
+      self.__real_get_access_token()
+    return self.__accessToken
+    
+  def run(self):
+    while(True):
+      if self.__leftTime > 10:
+        time.sleep(2)
+        self.__leftTime -= 2
+      else:
+        self.__real_get_access_token()
