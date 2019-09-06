13dbf34c08fa3e84896dc70332f55e8da41f6dcb
全王 <47687653+boristown@users.noreply.github.com>
2019-09-06 17:08:23 +0800

    迭代114：移动标注文字

--- ZeroAI.py
+++ ZeroAI.py
@@ -188,7 +188,7 @@ def chat(input_text):
             )
     #plt.hlines(bestvalue, 0, 10, colors = "c", linestyles = "dotted")
     #plt.vlines(bestindex, minvalue, maxvalue, colors = "c", linestyles = "dotted")
-    plt.annotate(output_text, xy=(bestindex, bestvalue), xytext=(bestindex, bestvalue / 2.0),
+    plt.annotate(output_text, xy=(bestindex, bestvalue), xytext=(bestindex - 1.8, bestvalue / 4.0),
                  arrowprops=dict(facecolor='black', shrink=0.05),
                 )
     plt.legend()
