edcf2ea3f499af2c029eaf81ce9fe607f6e16074
Boris Town <boristown@outlook.com>
2020-03-07 18:48:59 +0800

    迭代156：版本号分离

--- forcastline.py
+++ forcastline.py
@@ -1,9 +1,7 @@
 
 def get_version(input_text):
     if 'V1' in input_text:
-        input_text.replace("V1", "")
-        return input_text, "V1"
+        return input_text.replace("V1", "").strip(), "V1"
     if 'V2' in input_text:
-        input_text.replace("V2", "")
-        return input_text, "V2"
-    return input_text, "V1"
+        return input_text.replace("V2", "").strip(), "V2"
+    return input_text.strip(), "V1"
