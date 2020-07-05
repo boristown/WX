b1fd0f27a3824c400862551a4e67dca03dbf4719
Boris Town <boristown@outlook.com>
2020-07-05 19:04:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from handle import Handle
 
 urls = (
     '/wx', 'Handle',
+    '/','hello'
 )
+
+class hello:
+    def GET(self):
+        raiseweb.seeother('https://www.forcastline.com')
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
