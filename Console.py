befd21d41991b2e1c40e8ad6f7f5763b22b592b5
Boris Town <boristown@outlook.com>
2021-04-11 18:27:47 +0800

    性能分析

--- Console.py
+++ Console.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 # filename: Console.py
 
+import profile
 import ZeroAI
 import sys
 
 while True:
   print ('input :')
   value = sys.stdin.readline()
-  print (ZeroAI.chat(value))
+  #print (ZeroAI.chat(value))
+  profile.run("ZeroAI.chat(value)")
