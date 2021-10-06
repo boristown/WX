2e1f13b3792bc2cc4a49a0c0db2dff4184c04d26
Boris Town <boristown@outlook.com>
2021-10-06 22:39:21 +0800

    支持10个市场

--- predict_batch.py
+++ predict_batch.py
@@ -23,18 +23,17 @@ def side_text_zh(side):
 def get_batch_predict_info(group, prediction):
     strategies = prediction["strategies"]
     n=len(strategies)
-    if n>5: #截取前5个市场
-        n=5
+    if n>10: #截取前10个市场
+        n=10
         strategies = strategies[:5]
-    text="批量预测 by 预言家III\n"
-    text+="组：binance\n"
-    text+="排序/市场/方向/评分\n"
+    text=group+"批量预测 by 预言家III\n"
+    text+="按评分倒序排列\n"
+    text+="市场/方向/评分\n"
     for i,strategy in enumerate(strategies):
-        text+=str(i+1)+" "+ str(strategy["symbol"]).replace("/","")
+        text+=str(strategy["symbol"]).replace("/","")
         if group_set[group] == 5:
             text+="@"+str(strategy["exchange"])
-        text+=" " + side_text_zh(strategy["side"]) + " " + str(strategy["score"]) + "\n"
-    text +="提示Tips：\n"
+        text+=" " + side_text_zh(strategy["side"]) + " " + str(round(float(strategy["score"]),2)) + "\n"
     text +="输入市场名查询详细预测结果。\n"
-    text +="预测结果基于最近128日K线自动生成，仅供参考。\n"
+    text +="预测结果基于最近128日K线自动生成，仅供参考。"
     return text
