31d5c416b41351152be0a52e68a7d87d46c7643a
AI Era <47687653+boristown@users.noreply.github.com>
2022-12-25 22:35:17 +0800

    Update Binance.py

--- root/Binance.py
+++ root/Binance.py
@@ -1,11 +1,64 @@
 import requests
 import datetime
+from collections import *
 
+get_price_btc_ts = 0
+get_price_btc_price = 0
+
+def get_price_btc():
+    #增加缓存，同一秒钟内只获取一次价格
+    global get_price_btc_ts, get_price_btc_price
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    if timestamp == get_price_btc_ts:
+        return get_price_btc_price
+    else:
+        get_price_btc_ts = timestamp
+        # 获取当前BTCUSDT价格
+        url = 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT'
+        r = requests.get(url)
+        price = float(r.json()['price'])
+        get_price_btc_price = price
+        return price
+
+get_price_eth_ts = 0
+get_price_eth_price = 0
+
+def get_price_eth():
+    #增加缓存，同一秒钟内只获取一次价格
+    global get_price_eth_ts, get_price_eth_price
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    if timestamp == get_price_eth_ts:
+        return get_price_eth_price
+    else:
+        get_price_eth_ts = timestamp
+        # 获取当前ETHUSDT价格
+        url = 'https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT'
+        r = requests.get(url)
+        price = float(r.json()['price'])
+        get_price_eth_price = price
+        return price
+
+get_price_symbol_ts = defaultdict(int)
+get_price_symbol_price = {}
+
+def get_price_symbol(symbol):
+    #增加缓存，同一秒钟内只获取一次价格
+    global get_price_symbol_ts, get_price_symbol_price
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    if timestamp == get_price_symbol_ts[symbol]:
+        return get_price_symbol_price[symbol]
+    else:
+        get_price_symbol_ts[symbol] = timestamp
+        # 获取当前symbol价格
+        url = 'https://api.binance.com/api/v3/ticker/price?symbol=' + symbol
+        r = requests.get(url)
+        price = float(r.json()['price'])
+        get_price_symbol_price[symbol] = price
+        return price
+    
 def get_price():
     # 获取当前BTCUSDT价格
-    url = 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT'
-    r = requests.get(url)
-    price = float(r.json()['price'])
+    price = get_price_btc()
     #输出格式：Binance交易所的当前BTCUSDT价格为：price
     #北京时间：2020年12月25日 15:00:00
     return 'Binance交易所的当前BTCUSDT价格为：' + str(price) + '\n' +\
